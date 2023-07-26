0e5daca2a92cf909887764f1c8c971d0c6218e81
tushar <tushargadher123@gmail.com>
2023-07-26 18:38:00 +0530

    minor update

--- app/Component/InfoBox.js
+++ app/Component/InfoBox.js
@@ -1,49 +1,45 @@
+import BagImg from "../../Images/Bag.png";
+import StoreImg from "../../Images/Store.png";
+import VisitorImg from "../../Images/Visitor.png";
+import OrderImg from "../../Images/Order.png";
+import Image from "next/image";
 const TotalCount = () => {
   return (
     <>
-      {/* total sales */}
-      <div className="bg-white h-[91px] w-[265px] rounded-[10px] flex items-center justify-evenly">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          class="w-11 h-11 text-orange-600 "
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
-          />
-        </svg>
+      <div className="flex justify-between">
+        {/* total sales */}
+        <div className="bg-white h-[91px] w-[275px] rounded-[10px] flex items-center justify-evenly">
+          <Image src={BagImg} />
 
-        <div className=" w-[170px] h-[51px] flex flex-col justify-center">
-          <span className="font-light text-xs">Total Sales</span>
-          <span className="font-semibold text-[22px]"> &#36;2,456</span>
+          <div className=" w-[170px] h-[51px] flex flex-col justify-center">
+            <span className="font-light text-xs">Total Sales</span>
+            <span className="font-semibold text-[22px]"> &#36;2,456</span>
+          </div>
         </div>
-      </div>
 
-      {/* total expenses */}
-      <div className="bg-white h-[91px] w-[265px] rounded-[10px] flex items-center justify-evenly">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          class="w-11 h-11"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M13.5 21v-7.5a.75.75 0 01.75-.75h3a.75.75 0 01.75.75V21m-4.5 0H2.36m11.14 0H18m0 0h3.64m-1.39 0V9.349m-16.5 11.65V9.35m0 0a3.001 3.001 0 003.75-.615A2.993 2.993 0 009.75 9.75c.896 0 1.7-.393 2.25-1.016a2.993 2.993 0 002.25 1.016c.896 0 1.7-.393 2.25-1.016a3.001 3.001 0 003.75.614m-16.5 0a3.004 3.004 0 01-.621-4.72L4.318 3.44A1.5 1.5 0 015.378 3h13.243a1.5 1.5 0 011.06.44l1.19 1.189a3 3 0 01-.621 4.72m-13.5 8.65h3.75a.75.75 0 00.75-.75V13.5a.75.75 0 00-.75-.75H6.75a.75.75 0 00-.75.75v3.75c0 .415.336.75.75.75z"
-          />
-        </svg>
-
-        <div className=" w-[170px] h-[51px] flex flex-col justify-center">
-          <span className="font-light text-xs">Total Expenses</span>
-          <span className="font-semibold text-[22px]"> &#36;3,326</span>
+        {/* total expenses */}
+        <div className="bg-white h-[91px] w-[275px] rounded-[10px] flex items-center justify-evenly">
+          <Image src={StoreImg} />
+          <div className=" w-[170px] h-[51px] flex flex-col justify-center">
+            <span className="font-light text-xs">Total Expenses</span>
+            <span className="font-semibold text-[22px]"> &#36;3,326</span>
+          </div>
+        </div>
+        {/* total visitors */}
+        <div className="bg-white h-[91px] w-[275px] rounded-[10px] flex items-center justify-evenly">
+          <Image src={VisitorImg} />
+          <div className=" w-[170px] h-[51px] flex flex-col justify-center">
+            <span className="font-light text-xs">Total Visitors</span>
+            <span className="font-semibold text-[22px]">5,325</span>
+          </div>
+        </div>
+        {/* total Orders */}
+        <div className="bg-white h-[91px] w-[275px] rounded-[10px] flex items-center justify-evenly">
+          <Image src={OrderImg} />
+          <div className=" w-[170px] h-[51px] flex flex-col justify-center">
+            <span className="font-light text-xs">Total Ordesr</span>
+            <span className="font-semibold text-[22px]">3,326</span>
+          </div>
         </div>
       </div>
     </>
