fb06d8b1683fa2489dd256f0c706edd481e0d93a
tushar <tushargadher123@gmail.com>
2023-07-26 11:29:49 +0530

    login page completed

--- app/page.js
+++ app/page.js
@@ -3,22 +3,23 @@ import Image from "next/image";
 const Login = () => {
   return (
     <>
-      <div className="container h-screen bg-slate-100 flex justify-center items-center ">
+      <div className="container h-screen bg-slate-100 flex justify-center items-center">
         {/* inner box */}
 
-        <div className=" w-[900px] h-[564px] rounded-[10px] flex relative bg-white">
+        <div className="overflow-hidden w-[900px] h-[564px] rounded-[10px] flex relative bg-white">
           {/* left side image*/}
-          <div className=" h-full w-[50%]">
+          <div className=" h-full w-[50%] ">
             <Image src={LoginImg} alt="mountain" />
           </div>
           {/* right side form */}
-          <div className=" h-[484px] w-[370px] bg-white absolute top-[40px] left-[490px] flex flex-col justify-around">
-            <div className=" h-[57px] w-[261px]">
-              <h1 className="text-[22px]">Login</h1>
+          <div className=" h-[484px] w-[370px] bg-white absolute top-[40px] left-[490px] flex flex-col justify-around ">
+            <div className=" h-[57px] w-[261px] mb-[20px]">
+              <h1 className="text-[22px] font-semibold">Login</h1>
               <span className="text-[16px]">
                 Donec tortor quam at duis tortor.
               </span>
             </div>
+
             <div className=" h-[265px] w-[370px]">
               {/* input email */}
               <label class="block  h-[70px] mb-[5px]">
@@ -26,7 +27,7 @@ const Login = () => {
                   E-mail
                 </span>
                 <input
-                  type="text"
+                  type="email"
                   value="Placeholder Content"
                   class="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 text-neutral-300"
                 />
@@ -37,14 +38,14 @@ const Login = () => {
                   Password
                 </span>
                 <input
-                  type="password"
+                  type="text"
                   value="Placeholder Content"
                   class="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 text-neutral-300"
                 />
               </label>
               {/* forget password */}
               <div className="flex  justify-end flex-row mb-[25px]">
-                <span className="text-[14px] text-neutral-600">
+                <span className="text-[14px] text-neutral-600 font-semibold">
                   Forgot password?
                 </span>
               </div>
@@ -54,8 +55,12 @@ const Login = () => {
               </button>
             </div>
             <div className=" h-[82px] w-[370px]">
-              <h1>Login</h1>
-              <span>Donec tortor quam at duis tortor.</span>
+              <div className="my-4 flex items-center before:mt-0.5 before:flex-1 before:border-t before:border-neutral-200 after:mt-0.5 after:flex-1 after:border-t after:border-neutral-200">
+                <p class="mx-4 mb-0 text-center text-neutral-500 text-sm">or</p>
+              </div>
+              <button class="border border-neutral-600 bg-white text-neutral-600 py-2 px-4 rounded w-[370px] text-[16px] font-medium ">
+                Register now
+              </button>
             </div>
           </div>
         </div>
